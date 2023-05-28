359f4a444ae50bcf1f5f8778288594bb970551a8
sriram403 <78860461+sriram403@users.noreply.github.com>
2023-05-28 23:40:38 +0530

    added Readme file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from WackyWarper.Augmentor import Augmentor
 from WackyWarper import helper_functions
 
-list_of_directory = ['train']
+list_of_directory = ['Train']
 header_directory_name = "Augmented_Image"
 Number_Of_Images_Needed = 3
 
@@ -10,5 +10,5 @@ label_for_that = "" # give the label file path corresponding to the image
 
 if __name__ == "__main__":
     Augmentor.Start_Augmentor(list_of_directory, header_directory_name, Number_Of_Images_Needed)
-    # helper_functions.Visualize(image_to_look,label_for_that)
+    # helper_functions.Visualize(image_to_look,label_for_that) # to visualize the image
     pass
