cd5f8e1b57f893d13704ee87c66136e7091cb50d
sriram403 <78860461+sriram403@users.noreply.github.com>
2023-06-21 16:04:16 +0530

    Added Augmenting Empty Images

--- WackyWarper/Whole_PipeLine/__init__.py
+++ WackyWarper/Whole_PipeLine/__init__.py
@@ -1,6 +1,13 @@
 from WackyWarper.Augmentor import Augmentor
 from WackyWarper import helper_functions
 import shutil
+import os
+
+def check_and_fill_txt_file(file_path):
+    if os.path.exists(file_path):
+        if os.stat(file_path).st_size == 0:
+            with open(file_path, 'w') as file:
+                file.write("0 0 0 0 0")
 
 def Give_Me_Augmented_Data(IMG_DIR:dir, 
                            LABEL_DIR:dir, 
@@ -18,5 +25,12 @@ def Give_Me_Augmented_Data(IMG_DIR:dir,
     source_folder = ["Splitted/Valid","Splitted/Test"]
     for i in source_folder:
         shutil.move(i, AUGMENTED_HEADER_NAME)
+    
+    valid_files = os.listdir(AUGMENTED_HEADER_NAME+"/"+"Valid"+"/"+"labels")
+    test_files = os.listdir(AUGMENTED_HEADER_NAME+"/"+"Test"+"/"+"labels")
+    for i in valid_files:
+        check_and_fill_txt_file(os.path.join(AUGMENTED_HEADER_NAME,"Valid","labels",i))
+    for i in test_files:
+        check_and_fill_txt_file(os.path.join(AUGMENTED_HEADER_NAME,"Test","labels",i))
     print(">>I Finished All the Process<<")
     
